cf3504a80560d24e6fcb8d3f6c9de76006059b9e
isroScientist <67408817+isroScientist@users.noreply.github.com>
2021-01-05 13:33:12 +0530

    defined wishMe function

--- JARVIS/main.py
+++ JARVIS/main.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import datetime
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -8,5 +9,15 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning")
+    elif hour >= 12 and hour < 15:
+        speak("Good Afternoon")
+    
+    else:
+        speak("Good Evening!")
+
 if __name__ == "__main__":
-    speak("This is cool")
+    wishMe()
